95807e82daac3bb57038d96b9b81ba5583299b9d
xav168 <xav.ong.jh@gmail.com>
2023-10-28 13:18:03 -0700

    add date picker

--- aitinero/src/app/home/duration/page.tsx
+++ aitinero/src/app/home/duration/page.tsx
@@ -5,11 +5,13 @@ import { Button } from "@/components/ui/button";
 import FormWrapper from "@/components/FormWrapper";
 import useAppFormContext from "@/lib/hooks/useAppFormContext";
 import  {useRouter} from "next/navigation";
+import { DatePicker } from "@/components/ui/datePicker";
+import { Controller } from "react-hook-form";
 
 
 export default function DateForm() {
     const router = useRouter();
-    const {register, trigger, formState} = useAppFormContext();
+    const {control, trigger, formState} = useAppFormContext();
     const {isValid} = formState;
 
     return (
@@ -18,9 +20,28 @@ export default function DateForm() {
                 <div className="text-slate-700 text-xl font-semibold">
                 <p><strong>When</strong> do you want to go?</p>
                 </div>
-                <div className="text-white my-2 flex flex-row">
-                    <Input type="date" className="rounded-md p-2 w-24 bg-slate-800 mx-2" {...register('startDate')}/>
-                    <Input type="date" className="rounded-md p-2 w-24 bg-slate-800 mx-2" {...register('endDate')}/>
+                <div className="my-2 flex flex-row px-2">
+                    <div className="mx-2">
+                        <Controller
+                            control={control} 
+                            name = "startDate"
+                            render = {( {field: {onChange, value, ref}}) => (
+                        <DatePicker
+                        onChange={onChange}
+                        selected = {value}/>
+                            )}
+                        />
+                    </div>
+                    <div className="mx-2">
+                        <Controller
+                            control={control}
+                            name = "endDate"
+                            render = {({field: {onChange, onBlur, value, ref}}) => (
+                            <DatePicker
+                            onChange={onChange}
+                            selected = {value}/>)}
+                        />
+                    </div>
                 </div>
                 <div className="place-self-end">
                     <Button type="submit">
---
+++ aitinero/src/components/ui/datePicker.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import * as React from "react"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+export function DatePicker(props: {onChange: any, selected: Date}) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={"flex flex-row items-center"}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {props.selected ? format(props.selected, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={props.selected}
+          onSelect={props.onChange}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
