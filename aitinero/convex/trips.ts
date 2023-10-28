e35892f4aceb10bf4d8efca1dfa38567631d4606
Alvin Abraham <alvynben@gmail.com>
2023-10-28 12:08:22 -0700

    feat: add update and delete for trips

--- aitinero/convex/trips.ts
+++ aitinero/convex/trips.ts
@@ -1,4 +1,5 @@
 import { Infer, v } from "convex/values";
+import { TABLE_NAME } from "./schema";
 import { query, mutation } from "./_generated/server";
 
 // Constants
@@ -12,6 +13,16 @@ const createTripBody = v.object({
     location: v.string()
 });
 export type CreateTripBody = Infer<typeof createTripBody>;
+
+const updateTripBody = v.object({
+    id: v.id(TABLE_NAME.TRIPS),
+    start_date: v.optional(v.string()),
+    end_date: v.optional(v.string()),
+    name: v.optional(v.string()),
+    location: v.optional(v.string()),
+    events: v.optional(v.array(v.id(TABLE_NAME.EVENTS)))
+});
+export type UpdateTripBody = Infer<typeof updateTripBody>;
 
 // Functions
 export const create = mutation({
@@ -30,3 +41,21 @@ export const read = query({
         return ctx.db.get(id)
     },
 });
+
+export const update = mutation({
+    args: { body: updateTripBody },
+    handler: async (ctx, { body }) => {
+        const { id, ...rest } = body;
+        await ctx.db.patch(id, rest);
+
+        return ctx.db.get(id);
+    }
+})
+
+
+export const remove = mutation({
+    args: { id: v.id(TABLE_NAME.TRIPS) },
+    handler: async (ctx, { id }) => {
+        await ctx.db.delete(id);
+    }
+})
