80696d07194d8e2adf76d9953ff0c4922f263783
Lim Yu Zher <>
2023-10-28 01:16:59 -0700

    add schema

---
+++ aitinero/convex/schema.ts
@@ -0,0 +1,26 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export enum EVENT_STATUS {
+    Confirmed = "CONF",
+    Possible = "POSS"
+}
+
+export default defineSchema({
+    trips: defineTable({
+        start_date: v.string(),
+        end_date: v.string(),
+        name: v.string(),
+        location: v.string(),
+        events: v.array(v.id("events"))
+    }),
+    events: defineTable({
+        duration: v.string(), // in miliseconds from Unix Epoch
+        start_time: v.string(),
+        end_time: v.string(),
+        location: v.string(),
+        status: v.string(), // convert to enum
+        description: v.string(),
+        context: v.string()
+    })
+});
