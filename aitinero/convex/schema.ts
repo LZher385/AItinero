62fd49d0ce1ec3a0119aae2440a8fa02947cb396
xav168 <xav.ong.jh@gmail.com>
2023-10-29 10:47:27 -0700

    add preferences

--- aitinero/convex/schema.ts
+++ aitinero/convex/schema.ts
@@ -1,32 +1,36 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
+import { defineSchema, defineTable } from 'convex/server';
+import { v } from 'convex/values';
 
 export enum EVENT_STATUS {
-    Confirmed = "CONF",
-    Possible = "POSS"
+  Confirmed = 'CONF',
+  Possible = 'POSS'
 }
 
 export enum TABLE_NAME {
-    TRIPS = 'trips',
-    EVENTS = 'events'
+  TRIPS = 'trips',
+  EVENTS = 'events'
 }
 
 export default defineSchema({
-    trips: defineTable({
-        start_date: v.string(),
-        end_date: v.string(),
-        name: v.string(),
-        location: v.string(),
-        events: v.optional(v.array(v.id("events")))
-    }),
-    events: defineTable({
-        title: v.string(),
-        duration: v.string(), // in miliseconds from Unix Epoch
-        start_time: v.string(),
-        end_time: v.string(),
-        location: v.optional(v.string()),
-        status: v.union(v.literal(EVENT_STATUS.Confirmed), v.literal(EVENT_STATUS.Possible)),
-        description: v.optional(v.string()),
-        context: v.optional(v.string())
-    })
+  trips: defineTable({
+    start_date: v.string(),
+    end_date: v.string(),
+    name: v.string(),
+    location: v.string(),
+    preferences: v.string(),
+    events: v.optional(v.array(v.id('events')))
+  }),
+  events: defineTable({
+    title: v.string(),
+    duration: v.string(), // in miliseconds from Unix Epoch
+    start_time: v.string(),
+    end_time: v.string(),
+    location: v.optional(v.string()),
+    status: v.union(
+      v.literal(EVENT_STATUS.Confirmed),
+      v.literal(EVENT_STATUS.Possible)
+    ),
+    description: v.optional(v.string()),
+    context: v.optional(v.string())
+  })
 });
--- aitinero/src/app/home/preferences/page.tsx
+++ aitinero/src/app/home/preferences/page.tsx
@@ -9,11 +9,10 @@ import { useRouter } from 'next/navigation';
 export default function LocationForm() {
   const router = useRouter();
   const { register, trigger, formState } = useAppFormContext();
-  const { isValid } = formState;
   const validateStep = async () => {
-    let isValid = await trigger('location');
+    let isValid = await trigger('preferences');
     if (isValid) {
-      router.push('/home/preferences');
+      router.push('/home/duration');
     }
   };
 
@@ -22,14 +21,14 @@ export default function LocationForm() {
       <div className="flex flex-col place-items-center">
         <div className="text-xl font-semibold">
           <p>
-            <strong>Where</strong> do you want to go?
+            <strong>What</strong> kind of trip do you like?
           </p>
         </div>
         <div className="my-2">
           <Input
             type="text"
             className="rounded-md p-2 w-96"
-            {...register('location', {
+            {...register('preferences', {
               validate: (value) =>
                 value?.length > 0 && /^[a-zA-Z ]*$/.test(value)
             })}
@@ -37,7 +36,7 @@ export default function LocationForm() {
         </div>
         <div className="flex flex-row my-5">
           <div mx-10 className="mx-20">
-            <Button type="button" onClick={() => router.push('/home/')}>
+            <Button type="button" onClick={() => router.push('/home/location')}>
               Back
             </Button>
           </div>
