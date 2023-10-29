ac32f3f355ba750a9b8ad1323577a9ccf58f1ac0
HandmadeByAnu <anuradhamukherjee0@gmail.com>
2023-10-29 10:58:56 -0700

    completed chatbot functionality

--- aitinero/src/components/Chats/Chats.tsx
+++ aitinero/src/components/Chats/Chats.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Chats.scss";
+import { useQuery, useMutation, useAction } from 'convex/react'
+import { api } from '../../../convex/_generated/api'
+import { EVENT_STATUS, TABLE_NAME } from '../../../convex/schema';
+import { Id } from '../../../convex/_generated/dataModel';
 
 interface Props {
   userResponse: string;
@@ -11,6 +15,21 @@ interface Props {
   };
   sendUserResponse: string;
   optionClick: (ev: React.MouseEvent<HTMLElement>) => void;
+  dayarray: {
+            date: string;
+            events: ({
+                _id: Id<"events">;
+                _creationTime: number;
+                location?: string | undefined;
+                description?: string | undefined;
+                context?: string | undefined;
+                title: string;
+                duration: string;
+                start_time: string;
+                end_time: string;
+                status: EVENT_STATUS;
+            } | null)[];
+        }[] | undefined
 }
 
 interface MessagesInfo {
@@ -20,10 +39,18 @@ interface MessagesInfo {
   sender: string;
 }
 
+interface History {
+  role: "user" | "assistant" | "system",
+  content: string
+}
+
 const Chats: React.FC<Props> = props => {
   const [messages, setMessages] = useState<MessagesInfo[]>([]);
+  const [history, setHistory] = useState<History[]>([]);
   const dummyRef = useRef<HTMLDivElement>(null);
   const bodyRef = useRef<HTMLDivElement>(null);
+
+  const chat = useAction(api.openai.chat)
 
   // stacking up messages
   useEffect(() => {
@@ -32,20 +59,50 @@ const Chats: React.FC<Props> = props => {
         {
           purpose: "introduction",
           message:
-            "Hi there. If you're here, that means you're looking for a job. Tell me, what's your name?",
+            "Hi there! Ask me any questions you'd like.",
           sender: "bot"
         }
       ]);
+      setHistory([{
+        role: 'assistant',
+        content: "Hi there! Ask me any questions you'd like."
+      }])
     } else {
       let tempArray = [...messages];
+      let messageHistory = [...history];
       tempArray.push({ message: props.sendUserResponse, sender: "user" });
       setMessages(tempArray);
+      messageHistory.push({ content: props.sendUserResponse, role: "user" });
+      setHistory(messageHistory);
 
-      setTimeout(() => {
-        let temp2 = [...tempArray];
-        temp2.push(props.botResponse);
-        setMessages(temp2);
-      }, 1000);
+      const getResult = async () => {
+        const res = await chat( {messageHistory: history, messageBody: props.sendUserResponse, messageContext: JSON.stringify(props.dayarray)} )
+        console.log(res)
+        var lastresponse = res[messageHistory.length].content
+
+        console.log(lastresponse)
+
+        if (lastresponse == null) {
+          lastresponse = "Sorry, I was unable to understand that."
+        }
+
+        let temp2 = [...history];
+          temp2.push({content: props.sendUserResponse, role: "user"});
+          temp2.push({content: lastresponse, role: "assistant"});
+          setHistory(temp2);
+          let temp3 = [...messages];
+          temp3.push({message: props.sendUserResponse, sender: "user"});
+          temp3.push({message: lastresponse, sender: "bot"});
+          setMessages(temp3);
+      }
+
+      getResult()
+
+      
+
+      //do the send and getting here
+      
+      
     }
   }, [props.sendUserResponse, props.botResponse]);
 
